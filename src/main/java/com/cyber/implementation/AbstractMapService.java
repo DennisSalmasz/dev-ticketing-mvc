73c91017dbfb583488268bf7f6dedf9ab799f82c
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-10 00:19:23 +0000

    ticketing project - service implementation

---
+++ src/main/java/com/cyber/implementation/AbstractMapService.java
@@ -0,0 +1,48 @@
+package com.cyber.implementation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class AbstractMapService<T, ID> {
+    protected Map<ID, T> map=new HashMap<>();
+
+    T save(ID id, T object){
+        map.put(id, object);
+        return object;
+    }
+
+    List<T> findAll(){
+        return new ArrayList<>(map.values());
+    }
+
+    T findById(ID id){
+        return map.get(id);
+    }
+
+    void deleteById(ID id){
+        map.remove(id);
+    }
+
+    void delete(T Object){
+        map.entrySet().removeIf(entry -> entry.getValue().equals(Object));
+    }
+
+
+
+
+    /*
+    RoleDTO save(RoleDTO user);
+    RoleDTO findByID(Long id);
+    List<RoleDTO> findAll();
+    void delete(RoleDTO user);
+    void deleteByID(Long id);
+
+    UserDTO save(UserDTO user);
+    UserDTO findByID(String username);
+    List<UserDTO> findAll();
+    void delete(UserDTO user);
+    void deleteByID(String username);
+    */
+}
