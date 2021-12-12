a9dea199af9c3a12abbf8efed5cf7f6fe0ce6dcf
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-12 14:55:57 +0000

    task dto

--- src/main/java/com/cyber/controller/TaskController.java
+++ src/main/java/com/cyber/controller/TaskController.java
@@ -1,6 +1,8 @@
 package com.cyber.controller;
 
+import com.cyber.dto.TaskDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TaskController {
 
     @GetMapping("/create")
-    public String createTask(){
+    public String createTask(Model model){
+
+        model.addAttribute("task", new TaskDTO());
+
         return "/task/create";
     }
 }
